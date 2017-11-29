ecb6a01556157af2cd148642f8861330dd8675c2
Pål Simen Ruud <31000369+palsimen@users.noreply.github.com>
2017-11-29 18:44:58 +0100

    Start of a few scripts

---
+++ utils/check_process_and_action.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Checks if a process is running and applies an action if not.
+
+NAME="check_process_and_action.sh"
+INFO="INFO ($NAME): "
+ERROR="ERROR ($NAME): "
+
+function help {
+   echo ""
+   echo "$NAME - checks if process is running and applies an action if not"
+   echo ""
+   echo "   -p - process to search for"
+   echo "   -a - action to be executed if process if not running"
+   echo ""
+}
+
+process_name=""
+action=""
+while getopts ":p:a:" opt; do
+  case $opt in
+    p)
+      process_name="$OPTARG"
+      ;;
+    a)
+      action="$OPTARG"
+      ;;
+    *)
+      help
+      ;;
+  esac
+done
+
+if [ "x$process_name" == "x" ]; then
+   echo "$ERROR Need to provide a process name"
+   help
+   exit 1
+fi
+if [ "x$action" == "x" ]; then
+   echo "$ERROR Need to provide an action"
+   help
+   exit 1
+fi
+
+# Find pid of process_name
+pid=$(~/misc/bin/find_pid $process_name)
+
+echo $pid
+
