274bc43227b284b0c4ed25f2ab8e44f462392182
Pål Simen Ellingsen <31000369+palsimen@users.noreply.github.com>
2018-04-26 19:14:49 +0200

    SCript to check prices

---
+++ utils/price_checker.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+NAME="price-checker.sh"
+INFO="INFO ($NAME):"
+ERROR="ERROR ($NAME):"
+
+function Help {
+   echo ""
+   echo "$NAME"
+   echo ""
+   echo " Options:"
+   echo "   -m <mail>       - mail address"
+   echo ""
+}
+
+function SendMail() {
+   message="$1"
+   mail -s "$NAME" "$mail" <<< "$message"
+   echo "$INFO Mail is sent with this message: $message"
+}
+
+mail=""
+while getopts ":m:" opt; do
+   case $opt in
+      m)
+         mail="$OPTARG"
+         ;;
+      *)
+         Help
+         exit 1
+         ;;
+   esac
+done
+
+if [ "x$mail" == "x" ]; then
+   echo "$ERROR A mail address must be provided"
+   exit 1
+fi
+
+curl https://www.xxl.no/helly-hansen-packable-pant-skallbukse-herre/p/1070447_1_style > /tmp/xxl.txt
+
+price=$(grep -oP "<span class=\"price\">\K([0-9]+)" /tmp/xxl.txt)
+
+message=""
+if [ "$price" != 899 ]; then
+   message="$INFO Price has changed, new price=$price"
+else
+   message="$INFO Price has not changed, it is still $price"
+fi
+
+SendMail "$message"
